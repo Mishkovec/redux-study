027639b3f49ae0f1c693874f11857c91b0f845f7
Mishkovec <neilsa@mail.ru>
2019-11-02 14:04:31 +0300

    react and redux

--- redux-study/src/index.js
+++ redux-study/src/index.js
@@ -1,29 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createStore, bindActionCreators} from 'redux';
 import reducer  from './reducer';
-// import {ACT_INC, ACT_DEC} from './actions';
 import * as actions from './actions';
+import Counter from './counterBTNS';
+
 
 const store = createStore(reducer);
 const {dispatch} =store;
 
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// };
-
-// const incDispatch = ()=> dispatch(ACT_INC());
-// const decDispatch = ()=> dispatch(ACT_DEC());
-
 const {ACT_INC, ACT_DEC} = bindActionCreators(actions, dispatch);
   
-let inc =document.getElementById('inc');
-let dec =document.getElementById('dec');
-
-inc.addEventListener('click', ACT_INC);
-// dec.addEventListener('click', decDispatch);
-dec.onclick=ACT_DEC;
 
 const update = () => {
-    document.getElementById('counter')
-    .innerHTML=store.getState();
+    ReactDOM.render(
+        <Counter
+            count={store.getState()}
+            inc={ACT_INC}
+            dec={ACT_DEC}
+        />, 
+        document.getElementById('root')
+    );
 }
+update();
 store.subscribe(update);
+
