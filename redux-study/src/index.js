d4a16417fa915891118cebfab33c38340aa58701
Mishkovec <neilsa@mail.ru>
2019-11-02 13:43:47 +0300

    bindActionCreators

--- redux-study/src/index.js
+++ redux-study/src/index.js
@@ -1,19 +1,28 @@
-import {createStore} from 'redux';
+import {createStore, bindActionCreators} from 'redux';
 import reducer  from './reducer';
 import {ACT_INC, ACT_DEC} from './actions';
 
 const store = createStore(reducer);
 const {dispatch} =store;
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//     dispatch(creator(...args));
+// };
+
+// const incDispatch = ()=> dispatch(ACT_INC());
+// const decDispatch = ()=> dispatch(ACT_DEC());
+
+const {incDispatch, decDispatch} = bindActionCreators({
+    incDispatch: ACT_INC,
+    decDispatch: ACT_DEC
+}, dispatch);
   
 let inc =document.getElementById('inc');
 let dec =document.getElementById('dec');
 
-inc.onclick=()=>{
-    store.dispatch(ACT_INC());
-};
-dec.onclick=()=>{
-    store.dispatch(ACT_DEC());
-};
+inc.addEventListener('click', incDispatch);
+// dec.addEventListener('click', decDispatch);
+dec.onclick=decDispatch;
 
 const update = () => {
     document.getElementById('counter')
